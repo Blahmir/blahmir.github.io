de7f7faba3133ba74133840f63734f0e9464e532
blahmir <amirabdurazak@gmail.com>
2022-12-02 18:32:07 -0500

    project updates

--- data/projectsData.js
+++ data/projectsData.js
@@ -6,24 +6,24 @@ const projectsData = [
     href: '/blog/projects/oasis-pro',
   },
   {
-    title: 'Toy Surfboards',
-    description: `Creation & developpement of a e-commerce website.`,
-    imgSrc: '/static/images/projects/browser_toy.png',
-    href: '/blog/projects/toy-surfboards',
+    title: 'Gym Connect',
+    description: `Concurrent Fitness Centre System.`,
+    imgSrc: '/static/images/projects/gym-low-resolution-color-logo.png',
+    href: '/blog/projects/gym-connect',
   },
-  {
-    title: 'TotalEnergies',
-    description: `Creation of Guidelines for Small & Big Packaging.`,
-    imgSrc: '/static/images/projects/TE-Project-2022.png',
-    href: '/blog/projects/total-energies',
-  },
+  // {
+  //   title: 'TotalEnergies',
+  //   description: `Creation of Guidelines for Small & Big Packaging.`,
+  //   imgSrc: '/static/images/projects/TE-Project-2022.png',
+  //   href: '/blog/projects/total-energies',
+  // },
 
-  {
-    title: 'Havana Club x VALD',
-    description: `Operational launch of the Havana Club x VALD collaboration.`,
-    imgSrc: '/static/images/projects/havanaclub-vald.jpg',
-    href: '/blog/projects/havana-club-x-vald',
-  },
+  // {
+  //   title: 'Havana Club x VALD',
+  //   description: `Operational launch of the Havana Club x VALD collaboration.`,
+  //   imgSrc: '/static/images/projects/havanaclub-vald.jpg',
+  //   href: '/blog/projects/havana-club-x-vald',
+  // },
 ];
 
 export default projectsData;
